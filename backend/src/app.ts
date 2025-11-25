6cfbb4c56d3ea92c58a040dc0e9f5f295b6d5f25
Ahmad Fillah Alwy <ahmadfillahalwy12@gmail.com>
2025-11-25 22:05:35 +0700

    feat(BE): Implement seeding logic and machine api endpoints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,10 @@
 // src/app.ts
-
 import express from 'express';
 import cors from 'cors';
 
 // IMPORT ROUTER KITA
 import alertRoutes from './routes/alert.routes';
-// import machineRoutes from './routes/machine.routes'; // (Nyalakan ini nanti)
+import machineRoutes from './routes/machine.routes'; // (Nyalakan ini nanti)
 // import dashboardRoutes from './routes/dashboard.routes'; // (Nyalakan ini nanti)
 
 // BUAT APLIKASI
@@ -18,7 +17,7 @@ app.use(express.json()); // Izinkan server membaca JSON
 // PASANG ROUTER
 // Setiap request ke /api/alerts akan ditangani oleh alertRoutes
 app.use('/api/alerts', alertRoutes);
-// app.use('/api/machines', machineRoutes);
+app.use('/api/machines', machineRoutes);
 // app.Gunakan('/api/dashboard', dashboardRoutes);
 
 // EKSPOR APP (TANPA .listen())
---
+++ backend/src/routes/machine.routes.ts
@@ -0,0 +1,11 @@
+// backend/src/routes/machine.routes.ts
+import { Router } from 'express';
+import { getMachines, getMachineDetail, getMachineHistory } from '../controllers/machine.controller';
+
+const router = Router();
+
+router.get('/', getMachines);           // Akses: /api/machines
+router.get('/:id', getMachineDetail);   // Akses: /api/machines/1
+router.get('/:id/history', getMachineHistory); // Akses: /api/machines/1/history
+
+export default router;
