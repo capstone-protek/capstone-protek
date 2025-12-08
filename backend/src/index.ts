392c850335d993a2c8126eb195145bc6ade6537d
farisandim <farisandi458@gmail.com>
2025-12-08 19:45:51 +0700

    feat: Update server log message and enhance Swagger documentation for local development

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 4000;
 
 // JALANKAN SERVER
 app.listen(PORT, () => {
-  console.log(`🚀 Server BE Mock berjalan di http://localhost:${PORT}`);
+  console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
 });
